05257861e68263db8cea8a2e451729c8b918b5f5
joshweinmann <jweinmann25@gmail.com>
2025-07-27 21:41:55 -0400

    copilot toggle

--- lua/josh/plugins/copilot.lua
+++ lua/josh/plugins/copilot.lua
@@ -4,10 +4,22 @@ return {
     config = function()
       -- vim.g.copilot_no_tab_map = true
 
-      -- vim.keymap.set('i', '<C-J>', 'copilot#Accept("")', { expr = true, silent = true })
       vim.keymap.set('i', '<C-K>', 'copilot#Dismiss()', { expr = true, silent = true })
-      -- vim.keymap.set('i', '<C-Space>', 'copilot#GetCompleted()', { expr = true, silent = true })
-      -- vim.keymap.set('i', '<C-Tab>', 'copilot#CycleSuggestions()', { expr = true, silent = true })
+
+      -- toggle copilot
+      local copilot_enabled = true
+      function toggle_copilot()
+        if copilot_enabled then
+          vim.cmd('Copilot disable')
+          print("Copilot disabled")
+        else
+          vim.cmd('Copilot enable')
+          print("Copilot enabled")
+        end
+        copilot_enabled = not copilot_enabled
+      end
+
+      vim.keymap.set('n', '<leader>c', toggle_copilot, { desc = "Toggle Copilot", noremap = true, silent = true })
     end,
   }
 }
